5158d52730fb175476ba5636701237d669d7bf9e
Bruno Capuano <brunocapuano@microsoft.com>
2024-10-09 04:22:43 -0400

    Add new project for video analysis and update dependencies
    Updated ConsoleApp1.csproj to include new package references for AI, configuration, and OpenCV libraries. Reformatted and updated Labs.sln to include a new project, ConsoleOpenAI-04-VideoAnalyzer, and a new solution folder, MEAI. Added ConsoleOpenAI-04-VideoAnalyzer.csproj targeting .NET 8.0 with necessary package references and a video file for analysis. Implemented video processing and frame extraction using OpenCvSharp, and integrated OpenAI for video description generation.

---
+++ src/Labs/ConsoleOpenAI-04-VideoAnalyzer/Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+using OpenCvSharp;
+using OpenAI;
+using System.ClientModel;
+
+
+//////////////////////////////////////////////////////
+/// VIDEO
+//////////////////////////////////////////////////////
+
+// main settings
+var numberOfFrames = 15;
+var systemPrompt = @"You are a useful assistant. When you receive a group of images, they are frames of a unique video.";
+
+//var videoFileName = $"videos/firetruck.mp4";
+var videoFileName = $"videos/racoon.mp4";
+var userPrompt = @"The following frames represets a video. Describe the video.";
+
+//var videoFileName = $"videos/insurance_v3.mp4";
+//var userPrompt = @"You are an expert in evaluating car damage from car accidents for auto insurance reporting. 
+//Create an incident report for the accident shown in the video with 3 sections. 
+//- Section 1 will include the car details (license plate, car make, car model, approximant model year, color, mileage).
+//- Section 2 list the car damage, per damage in a list.
+//- Section 3 will only include exactly 6 sentence description of the car damage.";
+
+
+// Create or clear the "data" folder and the "data/frames" folder
+string dataFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "data");
+if (Directory.Exists(dataFolderPath))
+{
+    Directory.Delete(dataFolderPath, true);
+}
+Directory.CreateDirectory(dataFolderPath);
+Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "data/frames"));
+
+// video file
+string videoFile = Path.Combine(Directory.GetCurrentDirectory(), videoFileName);
+
+// Extract the frames from the video
+var video = new VideoCapture(videoFile);
+var frames = new List<Mat>();
+while (video.IsOpened())
+{
+    var frame = new Mat();
+    if (!video.Read(frame) || frame.Empty())
+        break;
+    // resize the frame to half of its size if the with is greater than 800
+    if (frame.Width > 800)
+    {
+        Cv2.Resize(frame, frame, new OpenCvSharp.Size(frame.Width / 2, frame.Height / 2));
+    }
+    frames.Add(frame);
+}
+video.Release();
+
+//////////////////////////////////////////////////////
+/// OPENAI
+//////////////////////////////////////////////////////
+#pragma warning disable OPENAI001 
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var openai_key = config["OPENAI_KEY"];
+
+OpenAIClient openAIClient = new(openai_key);
+ChatClient chatClient = openAIClient.GetChatClient("gpt-4o");
+
+List<ChatMessage> messages =
+[
+    new SystemChatMessage(systemPrompt),
+    new UserChatMessage(userPrompt),
+];
+
+
+// create the OpenAI files that represent the video frames
+int step = (int)Math.Ceiling((double)frames.Count / numberOfFrames);
+for (int i = 0; i < frames.Count; i += step)
+{
+    // save the frame to the "data/frames" folder
+    string framePath = Path.Combine(dataFolderPath, "frames", $"{i}.jpg");
+    Cv2.ImWrite(framePath, frames[i]);
+
+    // read the image bytes, create a new image content part and add it to the messages
+    var imageContentPart = ChatMessageContentPart.CreateImagePart(
+        imageBytes: BinaryData.FromBytes(File.ReadAllBytes(framePath)),
+        imageBytesMediaType: "image/jpeg");
+    var message = new UserChatMessage(imageContentPart);
+    messages.Add(message);
+}
+
+// send the messages to the assistant
+AsyncCollectionResult<StreamingChatCompletionUpdate> completionUpdates = chatClient.CompleteChatStreamingAsync(messages);
+
+// print the assistant responses
+Console.Write($"[ASSISTANT]: ");
+await foreach (StreamingChatCompletionUpdate completionUpdate in completionUpdates)
+{
+    if (completionUpdate.ContentUpdate.Count > 0)
+    {
+        Console.Write(completionUpdate.ContentUpdate[0].Text);
+    }
+}
